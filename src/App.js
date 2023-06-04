1c77489f7d42d2f19b42d4428f3915d1d58a2c7a
Sadia Malik <sadiaabida156@gmail.com>
2023-06-04 16:47:35 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Login from './Component/Login'
+
+
+
+function App(){
+  return(
+    <div>
+      
+ 
+    
+      {<Login/>}
+
+      
+    </div>
+  );
+}
+export default App;
+
